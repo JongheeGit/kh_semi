b0eec2b68a5e258daf1a6714737b222a96a23e6d
LeeJW98 <tst0579@gmail.com>
2024-10-23 14:40:41 +0900

    커밋

---
+++ emerMsgGit/src/main/java/com/emerMsg/demo/EmerMsgGitApplication.java
@@ -0,0 +1,13 @@
+package com.emerMsg.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmerMsgGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmerMsgGitApplication.class, args);
+	}
+
+}
---
+++ emerMsgGit/src/test/java/com/emerMsg/demo/EmerMsgGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emerMsg.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmerMsgGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
